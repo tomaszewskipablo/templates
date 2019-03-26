d2542de622aa9c9348b9907c3acf414894e2de17
Adam Wolny <32485175+adamwolny@users.noreply.github.com>
2019-03-26 17:33:48 +0100

    Update Zad5.cpp

--- Zadania/Zad5/Zad5.cpp
+++ Zadania/Zad5/Zad5.cpp
@@ -15,22 +15,10 @@ private:
 	double sumaDoubli;
 public:
 	Contener() :sumaIntow(0), sumaDoubli(0) {};
-	template<class T>
-	void addElement(T element) {
-		if (typeid(int) == typeid(T)) {
-			sumaIntow += element;
-			std::cout << "Dodano element o typie <int>" << std::endl;
-			std::cout << "Suma intow wynosi: " << sumaIntow << std::endl;
-		}
-		else if (typeid(double) == typeid(T)) {
-			sumaDoubli += element;
-			std::cout << "Dodano element o typie <double>" << std::endl;
-			std::cout << "Suma doubli wynosi: " << sumaDoubli << std::endl;
-		}
-		else
-		{
-			std::cout << "Proba dodania elementu o niewlasciwym typie" << std::endl;
-		};
+
+	
+	void addElement() {
+		
 	}
 };
 
